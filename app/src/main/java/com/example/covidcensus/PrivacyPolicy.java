b88c412caa522a29c28a39b4b0d841ade168bd1b
Shikhar <shikhar.sri111@gmail.com>
2021-06-29 00:34:08 +0530

    Added Privacy Policy

---
+++ app/src/main/java/com/example/covidcensus/PrivacyPolicy.java
@@ -0,0 +1,21 @@
+package com.example.covidcensus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class PrivacyPolicy extends AppCompatActivity {
+
+    WebView webView;
+    public String filename="covidcensusprivacy.html";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+        webView = (WebView) findViewById(R.id.privacyid);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/"+ filename);
+    }
+}
